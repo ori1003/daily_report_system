b43663a727dd1beac1073413e5f37e821d70363b
ori.c <ori.c@192.168.11.7>
2024-11-06 22:42:10 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.ReportService;
 
 //日報に関する処理を行うActionクラス
@@ -71,5 +73,77 @@ public class ReportAction extends ActionBase {
 
         //新規登録画面を表示
         forward(ForwardConst.FW_REP_NEW);
+    }
+
+    /**
+     * 新規登録を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void create() throws ServletException,IOException{
+        //CSRF対策用トークンのチェック
+        if(checkToken()) {
+            //日報の日付が入力されていなければ、今日の日付を設定
+            LocalDate day=null;
+            if(getRequestParam(AttributeConst.REP_DATE)==null
+                    ||getRequestParam(AttributeConst.REP_DATE).equals("")) {
+                day=LocalDate.now();
+            }else {
+                day=LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+            }
+
+            //セッションからログイン中の従業員情報を取得
+            EmployeeView ev=(EmployeeView)getSessionScope(AttributeConst.LOGIN_EMP);
+
+            //パラメータのああ値をもとに日報情報んインスタンスを作成
+            ReportView rv=new ReportView(
+                    null,
+                    ev,//ログインしている従業員を、日報作成者として登録
+                    day,
+                    getRequestParam(AttributeConst.REP_TITLE),
+                    getRequestParam(AttributeConst.REP_CONTENT),
+                    null,
+                    null);
+
+            //日報情報登録
+            List<String> errors=service.creat(rv);
+
+            if(errors.size()>0) {
+                //登録中にエラーがあった場合
+                putRequestScope(AttributeConst.TOKEN,getTokenId());//CSRF対策トークン
+                putRequestScope(AttributeConst.REPORT,rv);//入力された日報データ
+                putRequestScope(AttributeConst.ERR,errors);//エラーリスト
+
+                //新規作成画面を再表示
+                forward(ForwardConst.FW_REP_NEW);
+            }else {
+                //登録中にエラーがなかった場合
+
+                //セッションにフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH,MessageConst.I_REGISTERED.getMessage());
+
+                //一覧画面にリダイレクト
+                redirect(ForwardConst.ACT_REP,ForwardConst.CMD_INDEX);
+            }
+        }
+    }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException,IOException{
+        //idを条件に日報データを取得
+        ReportView rv=service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if(rv==null) {
+            //日報データが存在しない場合はエラー
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+        }else {
+            putRequestScope(AttributeConst.REPORT,rv);//取得した日報データ
+
+            forward(ForwardConst.FW_REP_SHOW);
+        }
     }
 }

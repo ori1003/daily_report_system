190b73b22fc165ddc7cefb6c29437976921dd406
ori.c <ori.c@192.168.11.7>
2024-11-07 20:34:54 +0900

    日報updateアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -146,4 +146,63 @@ public class ReportAction extends ActionBase {
             forward(ForwardConst.FW_REP_SHOW);
         }
     }
+
+    /**
+     * 編集画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void edit() throws ServletException,IOException{
+        //idを条件に日報データを取得
+        ReportView rv=service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        //セッションらかログイン中の従業員情報を取得
+        EmployeeView ev=(EmployeeView)getSessionScope(AttributeConst.LOGIN_EMP);
+
+        if(rv==null||ev.getId()!=rv.getEmployee().getId()) {
+            //日報データが存在しない、またはログイン中の従業員が作成者でない場合エラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+        }else {
+            putRequestScope(AttributeConst.TOKEN,getTokenId());//CSRF対策用トークン
+            putRequestScope(AttributeConst.REPORT,rv);//日報データ
+
+            //編集画面を表示
+            forward(ForwardConst.FW_REP_EDIT);
+        }
+    }
+
+    public void update() throws ServletException,IOException{
+        //CSRF対策用トークン
+        if(checkToken()) {
+            //idを条件に日報データを取得
+            ReportView rv=service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+            //入力された日報内容を設定する
+            rv.setReportDate(toLocalDate(getRequestParam(AttributeConst.REP_DATE)));
+            rv.setTitle(getRequestParam(AttributeConst.REP_TITLE));
+            rv.setContent(getRequestParam(AttributeConst.REP_CONTENT));
+
+            //日報データを更新する
+            List<String> errors=service.update(rv);
+
+            if(errors.size()>0) {
+                //更新中にエラーが発生した場合
+
+                putRequestScope(AttributeConst.TOKEN,getTokenId());
+                putRequestScope(AttributeConst.REPORT,rv);
+                putRequestScope(AttributeConst.ERR,errors);
+
+                //編集画面を再表示
+                forward(ForwardConst.FW_REP_EDIT);
+            }else {
+                //更新中にエラーなし
+
+                //セッションに更新完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH,MessageConst.I_UPDATED.getMessage());
+
+                //一覧画面にリダイレクト
+                redirect(ForwardConst.ACT_REP,ForwardConst.CMD_INDEX);
+            }
+        }
+    }
 }
